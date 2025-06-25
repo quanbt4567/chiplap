ef029bb4549e53b2f706d39ab156e75a531533e4
quanbt4567 <ntmquana23107@cusc.ctu.edu.vn>
2025-06-25 16:53:33 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!-- Trang chủ ChipLab -->
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>ChipLab.vn – Khám phá Công nghệ Bán Dẫn</title>
+</head>
+<body>
+    <h1>Chào mừng đến với ChipLab.vn!</h1>
+    <p>Nền tảng trải nghiệm – học tập – khám phá ngành công nghệ bán dẫn dành cho học sinh, sinh viên.</p>
+</body>
+</html>
